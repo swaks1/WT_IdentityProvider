b7d78082994ee0ff8f02d3f9db4de854b3f52b3b
Riste Poposki <rickyarpy@hotmail.com>
2018-08-29 18:57:54 +0200

    Initial Commit

--- WT_IdentityProvider/Program.cs
+++ WT_IdentityProvider/Program.cs
@@ -15,6 +15,7 @@ namespace WT_IdentityProvider
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+            //
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
